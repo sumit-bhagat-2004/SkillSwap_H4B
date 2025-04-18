33fc452885ec79e27cfb89f36ecf9a2240b8d405
Sowdarjya <kolaysowdarjya@gmail.com>
2025-04-18 19:34:51 +0530

    feat: integrate Cloudinary for image uploads and add multer middleware
    - Added Cloudinary configuration in a new cloudinary.js file.
    - Updated package.json and package-lock.json to include cloudinary, multer, and socket.io dependencies.
    - Implemented onboardUser controller to handle user onboarding and image uploads to Cloudinary.
    - Created multer middleware for handling file uploads.
    - Updated user routes to include onboarding functionality with file upload support.
    - Enhanced server setup to integrate Socket.io for real-time communication.

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import express from "express";
 import { clerkAuthMiddleware } from "../middlewares/clerkAuth.js";
-import { saveAuthenticatedUser } from "../controllers/user.controller.js";
+import {
+  onboardUser,
+  saveAuthenticatedUser,
+} from "../controllers/user.controller.js";
+import { requireAuth } from "@clerk/express";
+import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.get("/save", clerkAuthMiddleware, saveAuthenticatedUser);
+router.post("/save", clerkAuthMiddleware, saveAuthenticatedUser);
+router.put(
+  "/onboard-user",
+  clerkAuthMiddleware,
+  requireAuth(),
+  upload.array("certificates"),
+  onboardUser
+);
 
 export default router;
